d92fa87ac001c3b084e664eab0b47c8f393b02fc
smalaca <ab13.krakow@gmail.com>
2022-09-26 21:08:40 +0200

    Add additional rule TitleValidator

--- src/main/java/com/smalaca/conference/TitleValidator.java
+++ src/main/java/com/smalaca/conference/TitleValidator.java
@@ -13,5 +13,9 @@ class TitleValidator {
         if (title.length() < 8) {
             throw new IllegalArgumentException("Invalid title.");
         }
+
+        if (title.length() > 40) {
+            throw new IllegalArgumentException("Invalid title.");
+        }
     }
 }
